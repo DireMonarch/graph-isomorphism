89d075f34f94a074c2fae860a97ff61f96c3d31d
Jim Haslett <haslettj@gmail.com>
2025-01-09 16:20:55 -0500

    improved version of code on wikipedia

--- weisfeiler-leman-wikipedia/sample.py
+++ weisfeiler-leman-wikipedia/sample.py
@@ -3,68 +3,79 @@ g5_01 = {0: {3, 4}, 1: {2, 3, 4}, 2: {1, 3}, 3: {0, 1, 2}, 4: {0, 1}}
 g5_02 = {0: {1, 2, 4}, 1: {0, 3}, 2: {0, 3}, 3: {1, 2, 4}, 4: {0, 3}}
 
 
-def combineTwo(g1, g2):
-    g = {}
-    n = len(g1)
-    for node in g1:
-        s = set()
-        for neighbor in g1[node]:
-            s.add(neighbor)
-        g[node] = s.copy()
-    for node in g2:
-        s = set()
-        for neighbor in g2[node]:
-            s.add(neighbor + n)
-        g[node + n] = s.copy()
-    return g
+def wl_1(g1, g2):
+
+    def combineTwo(g1, g2):
+        g = {}
+        n = len(g1)
+        for node in g1:
+            s = set()
+            for neighbor in g1[node]:
+                s.add(neighbor)
+            g[node] = s.copy()
+        for node in g2:
+            s = set()
+            for neighbor in g2[node]:
+                s.add(neighbor + n)
+            g[node + n] = s.copy()
+        return g
 
 
-g = combineTwo(g5_00, g5_02)
-labels = {}
-glabels = {}
-for i in range(len(g)):
-    glabels[i] = 0
-glabelsCount = 1
-newlabel = 1
 
-done = False
-while not (done):
-    glabelsNew = {}
-    glabelsCountNew = 0
-    for node in g:
-        label = str(glabels[node])
-        s2 = []
-        for neighbor in g[node]:
-            s2.append(glabels[neighbor])
-        s2.sort()
-        for i in range(len(s2)):
-            label += "_" + str(s2[i])
-        if not (label in labels):
-            labels[label] = newlabel
-            newlabel += 1
-            glabelsCountNew += 1
-        glabelsNew[node] = labels[label]
-    if glabelsCount == glabelsCountNew:
-        done = True
-    else:
-        glabelsCount = glabelsCountNew
-        glabels = glabelsNew.copy()
-print(glabels)
 
-g0labels = []
-for i in range(len(g5_00)):
-    g0labels.append(glabels[i])
-g0labels.sort()
-certificate0 = ""
-for i in range(len(g5_00)):
-    certificate0 += str(g0labels[i]) + "_"
-g1labels = []
-for i in range(len(g5_01)):
-    g1labels.append(glabels[i + len(g5_00)])
-g1labels.sort()
-certificate1 = ""
-for i in range(len(g5_01)):
-    certificate1 += str(g1labels[i]) + "_"
+    g = combineTwo(g1, g2)
+    labels = {}
+    glabels = {}
+    for i in range(len(g)):
+        glabels[i] = 0
+    glabelsCount = 1
+    newlabel = 1
+
+
+
+
+
+    done = False
+    while not (done):
+        glabelsNew = {}
+        glabelsCountNew = 0
+        for node in g:
+            label = str(glabels[node])
+            s2 = []
+            for neighbor in g[node]:
+                s2.append(glabels[neighbor])
+            s2.sort()
+            for i in range(len(s2)):
+                label += "_" + str(s2[i])
+            if not (label in labels):
+                labels[label] = newlabel
+                newlabel += 1
+                glabelsCountNew += 1
+            glabelsNew[node] = labels[label]
+        if glabelsCount == glabelsCountNew:
+            done = True
+        else:
+            glabelsCount = glabelsCountNew
+            glabels = glabelsNew.copy()
+    print(glabels)
+
+    g0labels = []
+    for i in range(len(g1)):
+        g0labels.append(glabels[i])
+    g0labels.sort()
+    certificate0 = ""
+    for i in range(len(g1)):
+        certificate0 += str(g0labels[i]) + "_"
+    g1labels = []
+    for i in range(len(g2)):
+        g1labels.append(glabels[i + len(g1)])
+    g1labels.sort()
+    certificate1 = ""
+    for i in range(len(g2)):
+        certificate1 += str(g1labels[i]) + "_"
+    return certificate0, certificate1
+
+certificate0, certificate1 = wl_1(g5_00, g5_02)
 
 if certificate0 == certificate1:
     test = True
